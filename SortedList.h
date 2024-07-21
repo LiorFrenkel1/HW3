9a8754e6a73d39e0dc84443e57559fb467bbbe07
LiorFrenkel1 <liorfr5002@gmail.com>
2024-07-22 01:00:45 +0300

    Add insert

--- SortedList.h
+++ SortedList.h
@@ -4,12 +4,15 @@
 #include <stdexcept>
 
 namespace mtm {
+    typedef int T;
     //template <typename T>
     class SortedList {
     private:
-        int data;
+        T data;
+        int length;
         SortedList* next;
     public:
+        void printList();
         /**
          *
          * the class should support the following public interface:
@@ -25,7 +28,6 @@ namespace mtm {
          * 7. end method
          *
          * functions:
-         * 8. insert - inserts a new element to the list
          * 9. remove - removes an element from the list
          * 10. length - returns the number of elements in the list
          * 11. filter - returns a new list with elements that satisfy a given condition
@@ -43,13 +45,71 @@ namespace mtm {
           */
          SortedList(const SortedList& sortedList);
 
+         /**
+          * Adds a new type T element to the list in the right order
+          * @param element
+          */
+         void insert(T element);
     };
 
-    SortedList::SortedList() : data(NULL), next(nullptr) {};
+    SortedList::SortedList() : data(T()), length(0), next(nullptr) {};
 
-    SortedList::SortedList(const SortedList& sortedList) : data(sortedList.data),
-    next(sortedList.next){};
+    void SortedList::insert(T element) {
+        this->length++;
+        if (this->length == 1) {
+            this->data = element;
+            return;
+        }
+        SortedList* newNode = new SortedList();
+        T temp;
+        if (element > this->data) { // If the new element is the new biggest
+            temp = this->data;
+            this->data = element;
+            newNode->data = temp;
+            newNode->next = this->next;
+            this->next = newNode;
+            return;
+        }
+        newNode->data = element;
+        if (length == 2) {
+            this->next = newNode;
+            return;
+        }
+        SortedList* previous = this->next;
+        if (element > previous->data) { // If the new element is the new biggest
+            temp = previous->data;
+            previous->data = element;
+            newNode->data = temp;
+            newNode->next = previous->next;
+            previous->next = newNode;
+            return;
+        }
+        SortedList* current = previous->next;
+        for (int i = 3; i < this->length; i++) {
+            if (current->data < element) {
+                temp = current->data;
+                current->data = element;
+                newNode->data = temp;
+                newNode->next = current->next;
+                current->next = newNode;
+                return;
+            }
+            previous = previous->next;
+            current = current->next;
+        }
+        previous->next = newNode;
+    }
 
+    void SortedList::printList() {
+        std::cout << this->data << ' ';
+        SortedList* nextNode = this->next;
+        for (int i = 2; i <= this->length; i++) {
+            std::cout << nextNode->data << ' ';
+            nextNode = nextNode->next;
+        }
+    }
+
+    SortedList::SortedList(const SortedList &sortedList) =default;
 
     /*
     template <class T>
